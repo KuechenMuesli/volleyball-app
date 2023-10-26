9dff90989e66804bbef1e759466fcb7240c171d7
Paul <kuechenmuesli@gmail.com>
2023-10-26 21:55:25 +0200

    did some changes, removed console.log statements

--- src/app/components/main-menu/main-menu.component.ts
+++ src/app/components/main-menu/main-menu.component.ts
@@ -1,15 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-main-menu',
   templateUrl: './main-menu.component.html',
   styleUrls: ['./main-menu.component.css']
 })
-export class MainMenuComponent implements OnInit{
-  gridTileHeight: number = 200;
-  ngOnInit() {
-    console.log("HERE")
-    this.gridTileHeight = Math.round(700 * (window.innerHeight / window.innerWidth));
-    console.log(this.gridTileHeight);
-  }
+export class MainMenuComponent {
+
 }
